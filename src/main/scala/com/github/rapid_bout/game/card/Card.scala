7c9fa86e97ee07b8d886893f51f274b156c29dd0
Javakky <iemura.java@gmail.com>
2023-01-04 16:44:47 +0900

    docs: Card.scala に DocComment を追加

--- src/main/scala/com/github/rapid_bout/game/card/Card.scala
+++ src/main/scala/com/github/rapid_bout/game/card/Card.scala
@@ -5,9 +5,19 @@ import com.github.rapid_bout.game.effect.Term.AnyTime
 import com.github.rapid_bout.game.effect.{CounterTerm, Term}
 
 trait Card {
+
+  /** カードが表向きで存在する場合の打点 */
   val point: Int
+
+  /** カードを使うために満たすべき条件 */
   val term: Term = AnyTime
+
+  /** WIP: カードを対抗時に使うために満たすべき条件 (デフォルトでは対抗不可) */
   val counterTerm: CounterTerm = Forbidden
+
+  /** カードを使う際に支払うコスト */
   val preprocess: Effect = DoNothing
+
+  /** カードを使った際に実行される効果 */
   val effect: Effect = DoNothing
 }
